dd384c4e506194414247e5a9c3637613ef342c0e
Jung Young Hun <hujung0268@gamil.com>
2024-06-22 15:27:39 +0900

    스프링 자습한 겁니다

---
+++ sbb/src/main/java/com/mysite/sbb/HelloController.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	//컨트롤러 만들기
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello World";
+	}
+	
+}
