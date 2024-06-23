b3a25cff2aa96baed8eb09be6915379efb1ce81d
Jung Young Hun <hujung0268@gamil.com>
2024-06-23 19:43:33 +0900

    스프링 자습 내용입니다

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,21 +5,34 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 	//질문 목록 URL 매핑하기
 	//데이터를 템플릿에 전달하기
+	//컨트롤러에서 서비스 사용하기
 	
-	private final QuestionRepository questionRepository;
+//	private final QuestionRepository questionRepository;
+	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
-		List<Question> questionList = this.questionRepository.findAll();
+//		List<Question> questionList = this.questionRepository.findAll();
+		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
+	}
+	
+	@GetMapping(value = "/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
 	}
 }
