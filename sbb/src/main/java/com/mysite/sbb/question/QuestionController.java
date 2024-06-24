ef9b08a9d7aad1c987d5f4748be9bd9fddf1e107
Jung Young Hun <hujung0268@gamil.com>
2024-06-24 21:03:10 +0900

    스프링 자습내용

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -39,5 +41,12 @@ public class QuestionController {
 	@GetMapping("/create")
 	public String questionCreate() {
 		return "question_form";
+	}
+	
+	@PostMapping("/create")
+	public String questionCreate(@RequestParam(value="subject") String subject, 
+	@RequestParam(value="content") String content) {
+		//TODO: 질문을 저장한다
+		return "redirect:/question/list";
 	}
 }
