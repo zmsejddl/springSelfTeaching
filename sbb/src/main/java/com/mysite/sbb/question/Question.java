432fcd7e02d3ab3e276e69fb0368790d56a88863
Jung Young Hun <hujung0268@gamil.com>
2024-06-24 20:32:43 +0900

    스프링 자습 내용입니다

--- sbb/src/main/java/com/mysite/sbb/question/Question.java
+++ sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -31,7 +31,7 @@ public class Question {
 	@Column(columnDefinition = "TEXT")
 	private String content;
 	
-	private LocalDateTime createDateTime;
+	private LocalDateTime createDate;
 	
 	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
 	private List<Answer> answerList;
