202835e4be5d01828fff33494fc04bcea5fbbaca
Jung Young Hun <hujung0268@gamil.com>
2024-06-24 21:24:23 +0900

    스프링 자습 내용

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -41,7 +41,7 @@ public class QuestionController {
 	}
 	
 	@GetMapping("/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 	
