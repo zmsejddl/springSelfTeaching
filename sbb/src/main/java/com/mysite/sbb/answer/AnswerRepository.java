6af70c46b6350f4a36bcd0d53565eeff2121de62
Jung Young Hun <hujung0268@gamil.com>
2024-06-28 11:05:24 +0900

    다시 만듭니다. 어디서 꼬였는지 모르겠네요 ㅠㅠ

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mysite.sbb.answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer>{
-	//리포지터리 생성하기
-}
