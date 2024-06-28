4bc33068e9610b67d9f78d45ffb3a91de28f5ea3
Jung Young Hun <hujung0268@gamil.com>
2024-06-28 15:08:21 +0900

    템플릿 만들기

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
@@ -35,5 +37,11 @@ public class QuestionController {
 	@GetMapping("/create")
 	public String questionCreate() {
 		return "question_form";
+	}
+	
+	@PostMapping("/create")
+	public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
+		//TODO: 질문을 저장한다
+		return "redirect:/question/list";	//질문 저장 후 질문 목록으로 이동
 	}
 }
