e88f31d4a46f86e5f1f1c4e67d8ffd674a357474
Jung Young Hun <hujung0268@gamil.com>
2024-06-28 10:05:40 +0900

    스프링 자습 내용

--- sbb/src/main/java/com/mysite/sbb/answer/Answer.java
+++ sbb/src/main/java/com/mysite/sbb/answer/Answer.java
@@ -7,6 +7,7 @@ import com.mysite.sbb.question.Question;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
@@ -19,7 +20,7 @@ public class Answer {
 	//답변 엔티티 만들기
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Column(columnDefinition = "TEXT")
