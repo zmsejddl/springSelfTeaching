8fc658ff7813615b7e62d3ae0c6863e3b6b4d6a5
Jung Young Hun <hujung0268@gamil.com>
2024-06-28 11:24:35 +0900

    findAll 메서드

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -23,16 +23,10 @@ class SbbApplicationTests {
 	
 	@Test
 	void testJap() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
 		
-		Question q2 = new Question();
-		q2.setSubject("스프링 부트 모델 질문입니다");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 }
