1b02d68898a05de59e588d44f5b13b537fa4c550
Jung Young Hun <hujung0268@gamil.com>
2024-06-28 15:20:15 +0900

    템플릿 수정하기

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -37,7 +37,7 @@ public class QuestionController {
 	}
 	
 	@GetMapping("/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 	
