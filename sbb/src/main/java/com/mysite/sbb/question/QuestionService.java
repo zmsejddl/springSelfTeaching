3115f2929e42d8bb7f99cde0d45bb36e893cee11
Jung Young Hun <hujung0268@gamil.com>
2024-06-30 11:48:43 +0900

    게시물 오류 수정

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -25,7 +25,7 @@ public class QuestionService {
 	public Page<Question> getList(int page) {
 		List<Sort.Order> sorts = new ArrayList<>();
 		sorts.add(Sort.Order.desc("createDate"));
-		Pageable pageable = PageRequest.of(page, 10);
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
 		return this.questionRepository.findAll(pageable);
 	}
 	
